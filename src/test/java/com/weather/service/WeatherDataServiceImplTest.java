8db7ab2838a5d5e76b9837c554500229cfd09357
Purushothama Naidu <kpsnaidu@gmail.com>
2022-10-07 07:02:52 +0100

    Update WeatherDataServiceImplTest.java

--- src/test/java/com/weather/service/WeatherDataServiceImplTest.java
+++ src/test/java/com/weather/service/WeatherDataServiceImplTest.java
@@ -50,4 +50,87 @@ public class WeatherDataServiceImplTest {
         assertEquals("05:30 AM", currentWeatherDataVO.getSunriseTime());
         assertEquals("08:25 PM", currentWeatherDataVO.getSunsetTime());
     }
+    
+    
+    public class LocalDynamodb {
+    private DynamoDBProxyServer server;
+    private AmazonDynamoDB amazonDynamoDB;
+    private final String port =  getAvailablePort();
+
+    private WickesLogger logger = WickesLoggerFactory.getLogger(this.getClass());
+
+    public LocalDynamodb() throws Exception {
+        System.setProperty("sqlite4java.library.path", "native-libs");
+
+        amazonDynamoDB = AmazonDynamoDBClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("user", "pwd")))
+                .withEndpointConfiguration(new EndpointConfiguration("http://localhost:" + port, "local"))
+                .build();
+    }
+
+    @PostConstruct
+    public void before() throws Exception {
+            this.server = ServerRunner.createServerFromCommandLineArgs(new String[]{"-inMemory", "-port", port});
+            server.start();
+            logger.info("in memory dynamodb started");
+    }
+
+    @PreDestroy
+    protected void after() throws Exception {
+        if (server != null) {
+            server.stop();
+        }
+
+        logger.info("in memory dynamodb stopped");
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+
+    public void createDynamoDBTable(Class... clazzes) {
+        Arrays.stream(clazzes)
+                .forEach(clazz -> createDynamoDBTable(clazz, empty()));
+    }
+
+    public void createDynamoDBTableWithProjectAll(Class... clazzes) {
+        Arrays.stream(clazzes)
+                .forEach(clazz -> createDynamoDBTable(clazz,
+                        Optional.of(new Projection().withProjectionType(ProjectionType.ALL))));
+    }
+
+    public void createDynamoDBTable(Class clazz, Optional<Projection> projection) {
+        DynamoDBMapper dynamoDBMapper =  new DynamoDBMapper(amazonDynamoDB);
+        CreateTableRequest createTableRequest = dynamoDBMapper.generateCreateTableRequest(clazz);
+
+        ProvisionedThroughput provisionedThroughput = new ProvisionedThroughput().withReadCapacityUnits(1L)
+                .withWriteCapacityUnits(1L);
+        createTableRequest.withProvisionedThroughput(provisionedThroughput);
+
+        if (createTableRequest.getGlobalSecondaryIndexes() != null) {
+            for (GlobalSecondaryIndex index : createTableRequest.getGlobalSecondaryIndexes()) {
+                index.setProvisionedThroughput(provisionedThroughput);
+                projection.ifPresent(index::setProjection);
+            }
+        }
+
+        amazonDynamoDB.createTable(createTableRequest);
+    }
+
+    public AmazonDynamoDB getAmazonDynamoDB() {
+        return this.amazonDynamoDB;
+    }
+
+    private String getAvailablePort() {
+        try (final ServerSocket serverSocket = new ServerSocket(0)) {
+            return String.valueOf(serverSocket.getLocalPort());
+        } catch (IOException e) {
+            throw new RuntimeException("Available port was not found", e);
+        }
+    }
+}
+
+    
 }
